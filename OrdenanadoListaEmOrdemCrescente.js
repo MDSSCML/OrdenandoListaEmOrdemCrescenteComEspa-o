1219c2cd8a3d148875e9c16394089e55196cafdb
Márcio Trovão <mc872684@gmail.com>
2021-08-06 12:01:57 -0300

    Acertando erro na variável

--- OrdenanadoListaEmOrdemCrescente.js
+++ OrdenanadoListaEmOrdemCrescente.js
@@ -1,6 +1,6 @@
 // ordenando lista em ordem crescente dando espaço
          function orderNGroup(lista) {
-            lista = [...new Set(lista)].sort((a, b) => a - b).join("");
-            console.log(lista);
+            Lista = [...new Set(lista)].sort((a, b) => a - b).join("");
+            console.log(Lista);
         }
         orderNGroup([7, 3, 15, 0, -2, 6, -5, 100, 1, 14, -1, 100, 5]);
